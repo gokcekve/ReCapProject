e39a75f8d4a41e72dac29489d5a3df6b1bcd7f72
Gökçe Köküm <gokcekokum@gmail.com>
2021-05-06 00:06:30 +0300

    Add project files.

---
+++ ConsoleIU/Program.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleIU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());            
+
+            foreach (var c in carManager.GetAll())
+            {
+                Console.WriteLine(c.Description);
+            }
+
+            //TO ADD A NEW CAR:
+            /*
+            Car car = new Car()
+            {
+                Id = 7,
+                BrandId = 2,
+                ColorId = 5,
+                DailyPrice = 150,
+                Description = "family size",
+                ModelYear = "2019"
+            };
+                        
+            carManager.Add(car);
+            Console.WriteLine("\n**The new car is added.");
+            */       
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=1, ColorId=1, DailyPrice=100, ModelYear="2010", Description="standart model"},
+                new Car{Id=2, BrandId=1, ColorId=2, DailyPrice=120, ModelYear="2015", Description="front-wheel"},
+                new Car{Id=3, BrandId=2, ColorId=2, DailyPrice=150, ModelYear="2018", Description="8 seats"},
+                new Car{Id=4, BrandId=3, ColorId=1, DailyPrice=250, ModelYear="2020", Description="8 bass speakers"},
+                new Car{Id=5, BrandId=3, ColorId=3, DailyPrice=200, ModelYear="2019", Description="front-wheel"},
+                new Car{Id=6, BrandId=4, ColorId=4, DailyPrice=100, ModelYear="2013", Description="standar model"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete=_cars.SingleOrDefault(c=>c.Id==car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
